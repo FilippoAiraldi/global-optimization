cdf71783eea682c44f9a0d360cc1186dbe0e55a4
Filippo Airaldi <f.airaldi@tudelft.nl>
2023-04-28 18:20:19 +0200

    switched from julia to python

--- src/consts.jl
+++
@@ -1,4 +0,0 @@
-using Distances: SqEuclidean
-
-const δ = 1e-9  # small number to avoid nans in distance computations
-const sqeuclideandist = SqEuclidean()
--- src/util.jl
+++
@@ -1,5 +0,0 @@
-"""logrange(x1, x2, n)
-
-Creates a range from `x1` to `x2` of `n` logarithmically-spaced steps.
-"""
-logrange(x1, x2, n) = (10^y for y in range(log10(x1), log10(x2), length=n))
