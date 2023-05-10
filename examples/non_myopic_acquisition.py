d218acf3526fdf87aa5d6cc7e2967cdd218b7146
Filippo Airaldi <filippoairaldi@gmail.com>
2023-05-10 16:47:57 +0200

    improved example

--- examples/non_myopic_acquisition.py
+++ examples/non_myopic_acquisition.py
@@ -8,83 +8,94 @@ from pymoo.optimize import minimize
 from pymoo.problems.functional import FunctionalProblem
 
 from globopt.benchmarking.optimal_acquisition import optimal_acquisition
-from globopt.core.regression import Array, Idw, RegressorType, fit, predict
+from globopt.core.regression import Array, Rbf, RegressorType, fit, predict
 from globopt.myopic.acquisition import acquisition as myopic_acquisition
 from globopt.nonmyopic.acquisition import acquisition as nonmyopic_acquisition
 
 plt.style.use("bmh")
 
+# define the function and its domain
+xl, xu = -3, +3
+
 
 def f(x):
-    return np.sin(20 * x) + 20 * np.square(x - 0.3)
+    return (
+        (1 + x * np.sin(2 * x) * np.cos(3 * x) / (1 + x**2)) ** 2
+        + x**2 / 12
+        + x / 10
+    )
+
+
+# create functions for computing and optimizing the myopic and non-myopic acquisitions
 
 
 def compute_myopic_acquisition(
-    mdl: RegressorType, x: Array
+    x: Array, mdl: RegressorType, c1: float, c2: float
 ) -> tuple[Array, tuple[float, float]]:
-    # compute minimizer of acquisition function
     X, y = mdl.Xm_, mdl.ym_
     dym = y.max() - y.min()
     n_var = X.shape[-1]
 
-    # elementwise=False enables vectorized evaluation of acquisition function
     def obj(x_: Array) -> Array:
-        return myopic_acquisition(x_[np.newaxis], mdl, dym=dym, c1=1, c2=0.5)[0]
+        return myopic_acquisition(x_[np.newaxis], mdl, None, dym, c1, c2)[0]
 
-    problem = FunctionalProblem(n_var=n_var, objs=obj, xl=0, xu=1, elementwise=False)
-    res = minimize(problem, PSO(), verbose=True, seed=1).opt[0]
-    return obj(x[0]), (res.X, res.F)
+    problem = FunctionalProblem(n_var=n_var, objs=obj, xl=xl, xu=xu, elementwise=False)
+    res = minimize(problem, PSO(25), verbose=True, seed=1).opt[0]
+    a = obj(x[0])
+    return a, (res.X, res.F)
 
 
 def compute_nonmyopic_acquisition(
-    mdl: RegressorType, x: Array, h: int
+    x: Array, mdl: RegressorType, h: int, c1: float, c2: float, discount: float
 ) -> tuple[Array, tuple[float, float]]:
-    # compute minimizer of non-myopic acquisition function
     n_var = mdl.Xm_.shape[-1]
 
     def obj(x_: Array) -> Array:
         # transform x_ from (n_samples, n_var * h) to (n_samples, h, n_var)
         x_ = x_.reshape(-1, h, n_var)
-        return nonmyopic_acquisition(x_, mdl, c1=1, c2=0.5)
+        return nonmyopic_acquisition(x_, mdl, c1, c2, discount)
 
     problem = FunctionalProblem(
-        n_var=n_var * h, objs=obj, xl=0, xu=1, elementwise=False
+        n_var=n_var * h, objs=obj, xl=xl, xu=xu, elementwise=False
     )
     res = minimize(problem, PSO(25 * h), verbose=True, seed=1).opt[0]
-
-    return optimal_acquisition(x[0], mdl, h, c1=1, c2=0.5), (res.X[:n_var], res.F)
+    a = optimal_acquisition(x[0], mdl, h, c1=c1, c2=c2, brute_force=True, verbosity=10)
+    return a, (res.X[:n_var], res.F)
 
 
 # create data points - X has shape (batch, n_samples, n_var), where the batch dim can be
 # used to fit multiple models at once. Here, it is 1
-X = np.array([0.1141, 0.3706, 0.5602, 0.8178, 0.9441]).reshape(1, -1, 1)
+X = np.array([-2.62, -1.99, 0.14, 1.01, 2.62]).reshape(1, -1, 1)
 y = f(X)
+mdl = fit(Rbf("thinplatespline", 0.01), X, y)
 
 # compute myopic acquisition function
-mdl = fit(Idw(), X, y)
-x = np.linspace(0, 1, 60).reshape(1, -1, 1)
-myopic_results = compute_myopic_acquisition(mdl, x)
+c1 = 1.0
+c2 = 0.5
+x = np.linspace(-3, 3, 100).reshape(1, -1, 1)
+myopic_results = compute_myopic_acquisition(x, mdl, c1, c2)
 
 # compute non-myopic acquisition function
-horizon = 3
-nonmyopic_results = compute_nonmyopic_acquisition(mdl, x, horizon)
+horizon = 2
+discount = 1.0
+nonmyopic_results = compute_nonmyopic_acquisition(x, mdl, horizon, c1, c2, discount)
 
 # plot function and its estimate
-_, ax = plt.subplots(constrained_layout=True, figsize=(9, 4))
+_, ax = plt.subplots(constrained_layout=True, figsize=(5, 4))
 y_hat = predict(mdl, x)
 line = ax.plot(x.flatten(), f(x).flatten(), label=r"$f(x)$")[0]
-ax.plot(X.flatten(), y.flatten(), "o", label=None, color=line.get_color(), markersize=7)
+ax.plot(X.flatten(), y.flatten(), "o", label=None, color=line.get_color(), markersize=9)
 ax.plot(x.flatten(), y_hat.flatten(), label=r"$\hat{f}(x)$")
 
 # plot acquisition functions
 for (a, a_min), lbl in zip(
     (myopic_results, nonmyopic_results), ("Myopic", "Non-myopic")
 ):
-    line = ax.plot(x.flatten(), a.flatten(), "--", label=f"{lbl} $a(x)$")[0]
-    ax.plot(*a_min, "*", label=None, color=line.get_color(), markersize=12)
+    line = ax.plot(x.flatten(), a.flatten(), "--", lw=2.5, label=f"{lbl} $a(x)$")[0]
+    ax.plot(*a_min, "*", label=None, color=line.get_color(), markersize=17)
 
 # make plot pretty
-ax.set_xlim(0, 1)
+ax.set_xlim(x.min(), x.max())
 ax.set_xlabel("x")
 ax.legend()
 plt.show()
