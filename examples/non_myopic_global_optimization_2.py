47609ea1183c92e1dcd4f07ae9e5d23a9827c15a
Filippo Airaldi <filippoairaldi@gmail.com>
2023-06-07 12:02:50 +0200

    implemented rollout in nonmyopic acquisition algorithm

--- examples/non_myopic_global_optimization_2.py
+++ examples/non_myopic_global_optimization_2.py
@@ -5,12 +5,14 @@ Another example comparing the myopic and non-myopic GO algorithms.
 
 import matplotlib.pyplot as plt
 import numpy as np
+from joblib import Parallel
 from pymoo.optimize import minimize
 
 from globopt.core.problems import AnotherSimple1DProblem
 from globopt.core.regression import Idw, predict
+from globopt.myopic.acquisition import acquisition as myopic_acquisition
 from globopt.myopic.algorithm import GO
-from globopt.nonmyopic.acquisition import optimal_acquisition
+from globopt.nonmyopic.acquisition import acquisition as nonmyopic_acquisition
 from globopt.nonmyopic.algorithm import NonMyopicGO
 
 plt.style.use("bmh")
@@ -18,7 +20,7 @@ plt.style.use("bmh")
 
 # instantiate problem and create starting training data
 problem = AnotherSimple1DProblem()
-x0 = [0.19, 0.92]
+x0 = [0.08, 0.19, 0.92]
 
 # instantiate myopic and non-myopic algorithms and then run them on the problem
 kwargs = {
@@ -28,8 +30,7 @@ kwargs = {
     "c2": 1.4246,
 }
 algorithms = (
-    GO(**kwargs),  # type: ignore[arg-type]
-    NonMyopicGO(horizon=2, discount=1.0, **kwargs, shrink_horizon=True),
+    GO(**kwargs), NonMyopicGO(**kwargs, horizon=2, discount=1.0)  # type: ignore
 )
 ITERS = 4
 results = (
@@ -40,57 +41,78 @@ results = (
         verbose=True,
         seed=1,
         save_history=True,
+        copy_algorithm=False,
     )
     for algorithm in algorithms
 )
 
 # plot myopic and non-myopic results results
-x = np.linspace(*problem.bounds(), 50).reshape(-1, 1)  # type: ignore[call-overload]
-y = problem.evaluate(x)
-n_cols = min(ITERS, 4)
-n_rows = int(np.ceil(ITERS / n_cols)) * 2
-_, all_axs = plt.subplots(
-    n_rows, n_cols, constrained_layout=True, figsize=(2.5 * n_cols, 2 * n_rows)
-)
-for result, ylbl, axs in zip(results, ("Myopic", "Non-myopic"), np.split(all_axs, 2)):
-    axs = axs.flatten()
-    axs[0].set_ylabel(ylbl, fontsize=9)
-    for i, (ax, algo) in enumerate(zip(axs, result.history)):
-        # plot true function and current sampled points
-        Xm = algo.pop.get("X").reshape(-1, 1)
-        ym = algo.pop.get("F").reshape(-1)
-        c = ax.plot(x, y, label="$f(x)$")[0].get_color()
-        ax.plot(Xm, ym, "o", color=c, markersize=8)
+with Parallel(n_jobs=-1, batch_size=8, verbose=0) as parallel:
+    x = np.linspace(*problem.bounds(), 300).reshape(-1, 1)  # type: ignore
+    y = problem.evaluate(x).reshape(-1)
+    n_cols = min(ITERS, 4)
+    n_rows = int(np.ceil(ITERS / n_cols)) * 2
+    _, all_axs = plt.subplots(
+        n_rows, n_cols, constrained_layout=True, figsize=(2.5 * n_cols, 2 * n_rows)
+    )
+    for result, ylbl, axs in zip(
+        results, ("Myopic", "Non-myopic"), np.split(all_axs, 2)
+    ):
+        axs = axs.flatten()
+        axs[0].set_ylabel(ylbl, fontsize=9)
+        for i, (ax, algo) in enumerate(zip(axs, result.history)):
+            # plot true function and current sampled points
+            c = ax.plot(x.reshape(-1), y, label="$f(x)$")[0].get_color()
+            Xm, ym = algo.pop.get("X", "F")
+            ax.plot(Xm.reshape(-1), ym.reshape(-1), "o", color=c, markersize=8)
 
-        # plot current regression model's prediction
-        mdl = algo.regression
-        y_hat = predict(mdl, x[np.newaxis])[0]
-        ax.plot(x, y_hat, label=r"$\hat{f}(x)$")
+            # plot current regression model's prediction
+            mdl = algo.regression
+            y_hat = predict(mdl, x)
+            ax.plot(x.reshape(-1), y_hat, label=r"$\hat{f}(x)$")
 
-        # plot the optimal acquisition function and its minimum, or the best point found
-        # if the algorithm has terminated (to do this, we have to access the next
-        # iteration of the algorithm)
-        if i + 1 < len(result.history):
-            next_algo = result.history[i + 1]
-            h = getattr(next_algo, "horizon", 1)
-            a = optimal_acquisition(x, mdl, h, c1=algo.c1, c2=algo.c2, verbosity=0)
-            acq_min = next_algo.acquisition_min_res.opt.item()
-            p = acq_min.X[: problem.n_var], acq_min.F[: problem.n_var]
+            # plot the optimal acquisition function and its minimum, or the best point
+            # found if the algorithm has terminated (to do this, we have to access the
+            # next iteration of the algorithm)
+            if i + 1 < len(result.history):
+                next_algo = result.history[i + 1]
+                if isinstance(next_algo, NonMyopicGO):
+                    a = nonmyopic_acquisition(
+                        x,
+                        mdl,
+                        next_algo.horizon,
+                        next_algo.discount,
+                        next_algo.c1,
+                        next_algo.c2,
+                        None,
+                        problem.xl,
+                        problem.xu,
+                        parallel,
+                    )
+                else:
+                    a = myopic_acquisition(
+                        x, mdl, y_hat, None, next_algo.c1, next_algo.c2
+                    )
 
-            ax_ = ax.twinx()
-            c = ax_.plot(x[:, 0], a, "--", label="$a(x)$", color="C2")[0].get_color()
-            ax_.plot(*p, "*", markersize=13, color=c)
-            ax_.set_axis_off()
-            ylim = ax_.get_ylim()
-            ax_.set_ylim(ylim[0] - 0.1, ylim[1] + np.diff(ylim))
-        else:
-            ax.plot(*algo.opt.get("X", "F"), "*", markersize=17, color="C4")
+                acq_min = next_algo.acquisition_min_res.opt.item()
+                p = acq_min.X[: problem.n_var], acq_min.F[: problem.n_var]
 
-        # set axis limits and title
-        ax.set_xlim(0.001, 0.999)
-        ylim = (-0.9, 2.2)
-        ax.set_ylim(ylim[0] - np.diff(ylim) * 0.1, ylim[1])
-        ax.set_title(f"iteration {i + 1}, best cost = {ym.min():.4f}", fontsize=9)
-    for j in range(i + 1, axs.size):
-        axs[j].set_axis_off()
+                ax_ = ax.twinx()
+                c = ax_.plot(x[:, 0], a, "--", label="$a(x)$", color="C2")[
+                    0
+                ].get_color()
+                ax_.plot(*p, "*", markersize=13, color=c)
+                ax_.set_axis_off()
+                ylim = ax_.get_ylim()
+                ax_.set_ylim(ylim[0] - 0.1, ylim[1] + np.diff(ylim) * 0.7)
+            else:
+                ax.plot(*algo.opt.get("X", "F"), "*", markersize=17, color="C4")
+
+            # set axis limits and title
+            ax.set_xlim(0.001, 0.999)
+            ylim = (-0.9, 2.2)
+            ax.set_ylim(ylim[0] - np.diff(ylim) * 0.1, ylim[1])
+            ax.set_title(f"iteration {i + 1}, best cost = {ym.min():.4f}", fontsize=9)
+        for j in range(i + 1, axs.size):
+            axs[j].set_axis_off()
 plt.show()
--- src/globopt/nonmyopic/algorithm.py
+++ src/globopt/nonmyopic/algorithm.py
@@ -8,11 +8,14 @@ References
     functions. Computational Optimization and Applications, 77(2):571–595, 2020
 """
 
-from typing import Any
+from typing import Any, Optional
 
-import numpy as np
+from joblib import Parallel
+from pymoo.algorithms.soo.nonconvex.pso import PSO
+from pymoo.core.algorithm import Algorithm
 from pymoo.core.problem import Problem
 from pymoo.problems.functional import FunctionalProblem
+from pymoo.termination.default import DefaultSingleObjectiveTermination
 
 from globopt.myopic.algorithm import GO, Result
 from globopt.nonmyopic.acquisition import Array, acquisition
@@ -25,8 +28,11 @@ class NonMyopicGO(GO):
         self,
         *args: Any,
         horizon: int,
-        shrink_horizon: bool = False,
         discount: float = 1.0,
+        shrink_horizon: bool = True,
+        rollout_algorithm: Optional[Algorithm] = None,
+        acquisition_rollout_kwargs: Optional[dict[str, Any]] = None,
+        n_jobs: Optional[int] = -1,
         **kwargs: Any,
     ) -> None:
         """instiantiate a new non-myopic GO algorithm.
@@ -36,23 +42,51 @@ class NonMyopicGO(GO):
         horizon : int
             Length of the lookahead predict horizon, which grants non-myopia to the
             algorithm. If `horizon=1`, it falls back to the myopic case.
+        discount : float, optional
+            Discount factor for the lookahead horizon. By default, `1.0`.
         shrink_horizon : bool, optional
             Whether to shrink the horizon when the algorithm is close to reaching the
             maximum number of iterations, if this limit is specified in the termination.
-            By default, `False`.
-        discount : float, optional
-            Discount factor for the lookahead horizon. By default, `1.0`.
+            By default, `True`.
+        rollout_algorithm : Algorithm, optional
+            Algorithm to use for the rollout phase of the non-myopic algorithm. By
+            default, `PSO` is used.
+        acquisition_rollout_kwargs : dict[str, Any], optional
+            Keyword arguments to pass to the `minimize` function of the rollout base
+            policy.
+        n_jobs : int, optional
+            Number of parallel jobs to use for the rollout phase. By default, `-1`.
         args, kwargs
             See `globopt.myopic.algorithm.GO`.
         """
         super().__init__(*args, **kwargs)
         self.horizon = horizon
-        self.shrink_horizon = shrink_horizon
         self.discount = discount
+        self.shrink_horizon = shrink_horizon
+        self.rollout_algorithm = rollout_algorithm
+        self.acquisition_rollout_kwargs = acquisition_rollout_kwargs or {}
+        self.n_jobs = n_jobs
 
-    def _internal_setup(self, problem: Problem) -> None:
-        super()._internal_setup(problem)
-        self.acquisition_fun_kwargs["discount"] = self.discount
+    def _setup(self, problem: Problem, **kwargs: Any) -> None:
+        super()._setup(problem, **kwargs)
+
+        if self.rollout_algorithm is None:
+            self.rollout_algorithm = PSO()
+        if hasattr(self.rollout_algorithm, "pop_size"):
+            self.rollout_algorithm.pop_size = (
+                self.rollout_algorithm.pop_size * problem.n_var
+            )
+
+        if "termination" not in self.acquisition_rollout_kwargs:
+            self.acquisition_rollout_kwargs[
+                "termination"
+            ] = DefaultSingleObjectiveTermination(ftol=1e-4, n_max_gen=300, period=10)
+
+        self.parallel = Parallel(self.n_jobs, verbose=0)
+        self.parallel.__enter__()
+
+    def _finalize(self):
+        self.parallel.__exit__(None, None, None)
 
     def _get_acquisition_problem(self) -> Problem:
         # shrink horizon if needed
@@ -62,17 +96,21 @@ class NonMyopicGO(GO):
 
         # define acquisition function problem
         problem: Problem = self.problem
-        n_var = problem.n_var
-
-        def obj(x: Array) -> Array:
-            # add batch axis to x
-            return acquisition(
-                x[np.newaxis], self.regression, h, **self.acquisition_fun_kwargs
-            )
-
         return FunctionalProblem(
-            n_var=n_var,
-            objs=obj,
+            problem.n_var,
+            lambda x: acquisition(
+                x,
+                self.regression,
+                h,
+                self.discount,
+                self.c1,
+                self.c2,
+                self.rollout_algorithm,
+                problem.xl,
+                problem.xu,
+                self.parallel,
+                **self.acquisition_rollout_kwargs,
+            ),
             xl=problem.xl,
             xu=problem.xu,
             elementwise=False,  # enables vectorized evaluation of acquisition function
@@ -82,3 +120,9 @@ class NonMyopicGO(GO):
         # from the optimal trajectory, take only the first point
         n_var = self.problem.n_var
         return result.X[:n_var].reshape(1, n_var)
+
+    def __getstate__(self) -> dict[str, Any]:
+        # joblib.Parallel cannot be deepcopied
+        state = self.__dict__.copy()
+        state.pop("parallel", None)
+        return state
