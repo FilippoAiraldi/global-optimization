4ed989069314ff619af4649823a964c4174d508e
Filippo Airaldi <filippoairaldi@gmail.com>
2023-06-16 11:43:38 +0200

    added docstring

--- src/globopt/myopic/algorithm2.py
+++ src/globopt/myopic/algorithm2.py
@@ -34,7 +34,7 @@ def _fit_mdl_to_init_points(
     X0: Union[int, Array2d],
     lhs_sampler: LatinHypercube,
 ) -> Union[Idw, Rbf]:
-    """Sample, if necessary, the initial points, evaluate them and fits the regression
+    """Samples, if necessary, the initial points, evaluate them and fits the regression
     model to them."""
     if isinstance(X0, int):
         X0 = (lb + (ub - lb) * lhs_sampler.random(X0))[np.newaxis]
@@ -51,7 +51,7 @@ def _setup_vpso(
     verbosity: int,
     pso_kwargs: Optional[dict[str, Any]],
 ) -> tuple[Array2d, Array2d, dict[str, Any], Optional[int]]:
-    """Setup the bounds and kwargs for the VPSO algorithm."""
+    """Sets up the bounds and kwargs for the VPSO algorithm."""
     lb = lb[np.newaxis]
     ub = ub[np.newaxis]
     if pso_kwargs is None:
@@ -79,6 +79,56 @@ def go(
     #
     pso_kwargs: Optional[dict[str, Any]] = None,
 ) -> tuple[Array1d, float]:
+    """Global Optimization (GO) based on RBF or IDW regression [1].
+
+    Parameters
+    ----------
+    func : callable with 2d array adn returns array_like
+        The (possibly, unknown) function to be minimized via GO. It must take as input
+        a 2d array of shape `(n_points, dim)` and return a 1d array_like of shape. The
+        input represents the points at which the function must be evaluated, while the
+        output is the value of the function at those points.
+    lb, ub : 1d array
+        Lower and upper bounds of the search space. The dimensionality of the search
+        space is inferred from the size of `lb` and `ub`.
+    mdl : Idw or Rbf
+        The regression model to be used in the approximation of the unknown function.
+    init_points : int or 2d array, optional
+        The initial points of the problem. If an integer is passed, the points are
+        sampled via latin hypercube sampling over the search space. If a 2d array is
+        passed, then these are used as initial points. By default, `5`.
+    c1 : float, optional
+        Weight of the contribution of the variance function in the algorithm's
+        acquisition function, by default `1.5078`.
+    c2 : float, optional
+        Weight of the contribution of the distance function in the algorithm's
+        acquisition function, by default `1.4246`.
+    maxiter : int, optional
+        Maximum number of iterations to run the algorithm for, by default `50`.
+    seed : int, optional
+        Seed for the random number generator, by default `None`.
+    verbosity : int, optional
+        Logger verbosity, by default `logging.WARNING`.
+    callback : callable with {"go", "nmgo"} and dict, optional
+        A callback function called before the first and at the end of each iteration.
+        It must take as input a string indicating the current algorithm and a dictionary
+        with the local variables of the algorithm. By default, `None`.
+    pso_kwargs : dict, optional
+        Optional keyword arguments to be passed to the `vpso` algorithm used to optimize
+        the acquisition function.
+
+    Returns
+    -------
+    tuple of 1d array and float
+        Returns a tuple containing the best minimizer and minimum found by the end of
+        the iterations.
+
+    References
+    ----------
+    [1] A. Bemporad. Global optimization via inverse distance weighting and radial basis
+        functions. Computational Optimization and Applications, 77(2):571–595, 2020
+    """
+
     logger.setLevel(verbosity)
 
     # add initial points to regression model
