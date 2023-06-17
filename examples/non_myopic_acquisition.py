5e5b299e9a13bd16b886641d6c01d7060b648da9
Filippo Airaldi <f.airaldi@tudelft.nl>
2023-06-17 13:38:59 +0200

    removed the mpc helper

--- examples/non_myopic_acquisition.py
+++ examples/non_myopic_acquisition.py
@@ -1,55 +1,21 @@
 """Example application of the non-myopic acquisition function."""
 
 
-from typing import Literal
-
 import matplotlib.pyplot as plt
 import numpy as np
-from vpso.typing import Array1d, Array3d
 
 from globopt.core.problems import Simple1dProblem
-from globopt.core.regression import Rbf, RegressorType, fit, predict
+from globopt.core.regression import Rbf, fit, predict
 from globopt.myopic.acquisition import acquisition as myopic_acquisition
 from globopt.nonmyopic.acquisition import (
     deterministic_acquisition as nonmyopic_deterministic_acquisition,
 )
-from globopt.nonmyopic.helpers import mpc_deterministic_acquisition_brute_force
 
 plt.style.use("bmh")
 
 # define the function and its domain
 lb, ub = np.array([-3.0]), np.array([+3.0])
 f = Simple1dProblem.f
-
-
-# create functions for computing and optimizing the myopic and non-myopic acquisitions
-
-
-def compute_myopic_acquisition(
-    x: Array3d, mdl: RegressorType, c1: float, c2: float
-) -> Array1d:
-    dym = mdl.ym_.ptp(1, keepdims=True)
-    a = myopic_acquisition(x.transpose(1, 0, 2), mdl, c1, c2, None, dym)
-    return a.squeeze()
-
-
-def compute_deterministic_nonmyopic_acquisition(
-    x: Array3d,
-    mdl: RegressorType,
-    h: int,
-    c1: float,
-    c2: float,
-    discount: float,
-    type: Literal["rollout", "mpc"],
-) -> Array1d:
-    if type == "mpc":
-        return mpc_deterministic_acquisition_brute_force(
-            x, mdl, h, discount, c1, c2, 100
-        )
-    return nonmyopic_deterministic_acquisition(
-        x, mdl, h, discount, c1, c2, "rollout", lb=lb, ub=ub
-    )
-
 
 # create data points - X has shape (batch, n_samples, dim), with batch=1
 X = np.array([-2.62, -1.99, 0.14, 1.01, 2.62]).reshape(1, -1, 1)
@@ -60,14 +26,14 @@ mdl = fit(Rbf("thinplatespline", 0.01), X, y)
 c1 = 1.0
 c2 = 0.5
 x = np.linspace(lb, ub, 100).reshape(-1, 1, 1)
-myopic_a = compute_myopic_acquisition(x, mdl, c1, c2)
+myopic_a = myopic_acquisition(x.transpose(1, 0, 2), mdl, c1, c2).squeeze()
 
 # compute deterministic non-myopic acquisition function
 horizon = 3
 discount = 1.0
-nonmyopic_deterministic_a = compute_deterministic_nonmyopic_acquisition(
-    x, mdl, horizon, c1, c2, discount, "rollout"
-)
+nonmyopic_deterministic_a = nonmyopic_deterministic_acquisition(
+    x, mdl, horizon, discount, c1, c2, "rollout", lb=lb, ub=ub
+).squeeze()
 
 
 # TODO: compute MC non-myopic acquisition function
