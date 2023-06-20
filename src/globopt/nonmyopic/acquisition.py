73564476a064872095915d155c9dea6db4e34f45
Filippo Airaldi <filippoairaldi@gmail.com>
2023-06-20 18:20:13 +0200

    implemented quasi MC, antithetic variates, CRN

--- src/globopt/nonmyopic/acquisition.py
+++ src/globopt/nonmyopic/acquisition.py
@@ -8,13 +8,11 @@ References
     functions. Computational Optimization and Applications, 77(2):571–595, 2020
 """
 
-
 from typing import Any, Literal, Optional, Union
 
 import numba as nb
 import numpy as np
-
-# from scipy.stats.qmc import MultivariateNormalQMC
+from scipy.stats.qmc import MultivariateNormalQMC
 from vpso import vpso
 from vpso.typing import Array1d, Array2d, Array3d
 
@@ -27,6 +25,9 @@ from globopt.core.regression import (
 )
 from globopt.myopic.acquisition import _idw_variance, _idw_weighting
 from globopt.myopic.acquisition import acquisition as myopic_acquisition
+
+"""Seed that is used for COMMON random numbers generation."""
+FIXED_SEED = 1909
 
 
 @nb.njit(
@@ -244,6 +245,24 @@ def deterministic_acquisition(
     )
 
 
+def _draw_normal(
+    mc_iters: int,
+    horizon: int,
+    n_samples: int,
+    quasi_mc: bool,
+    np_random: np.random.Generator,
+) -> Array3d:
+    """Draws a (quasi) random sample from the standard normal distribution."""
+    if not quasi_mc:
+        np_random.standard_normal((mc_iters, horizon, n_samples))
+
+    qmc_sampler = MultivariateNormalQMC(
+        mean=np.zeros(horizon * n_samples), inv_transform=False, seed=np_random
+    )
+    n_ = 2 ** int(np.ceil(np.log2(mc_iters)))
+    return qmc_sampler.random(n_)[:mc_iters].reshape((mc_iters, horizon, n_samples))
+
+
 def acquisition(
     x: Array3d,
     mdl: RegressorType,
@@ -252,76 +271,53 @@ def acquisition(
     c1: float = 1.5078,
     c2: float = 1.4246,
     type: Literal["rollout", "mpc"] = "rollout",
+    lb: Optional[Array1d] = None,  # only when `type == "rollout"`
+    ub: Optional[Array1d] = None,  # only when `type == "rollout"`
     #
-    deterministic: bool = False,
-    # mc_iters: int = 1024,
-    # quasi_monte_carlo: bool = True,
-    # common_random_numbers: bool = True,
+    mc_iters: int = 1024,
+    quasi_mc: bool = True,
+    common_random_numbers: bool = True,
+    antithetic_variates: bool = True,
     # control_variate: bool = True,
     #
-    ub: Optional[Array1d] = None,  # only when `type == "rollout"`
-    lb: Optional[Array1d] = None,  # only when `type == "rollout"`
-    pso_kwarg: Optional[dict[str, Any]] = None,
-    #
-    # seed: Optional[int] = None,
-    check: bool = True,  # TODO: set to False
-) -> Array3d:
+    pso_kwargs: Optional[dict[str, Any]] = None,
+    seed: Optional[int] = None,
+    check: bool = True,
+) -> Array1d:
     # TODO: write doc
+    if pso_kwargs is None:
+        pso_kwargs = {}
     if check:
-        assert mdl.Xm_.shape[0] == 1, "regression model must be non-batched"
-        if type == "rollout":
-            assert (
-                ub is not None and lb is not None
-            ), "upper and lower bounds must be provided for rollout"
-            assert x.shape[1] == 1, "x must have only one time step for rollout"
-        else:
-            assert (
-                x.shape[1] == horizon
-            ), "x must have the same number of time steps as the horizon"
-    if pso_kwarg is None:
-        pso_kwarg = {}
+        _check_args(x, mdl.Xm_.shape[0], horizon, type, lb, ub)
+    n_samples, mdl, lb, ub, rollout = _initialize_mdl_and_bounds(x, mdl, type, lb, ub)
 
-    # initialize quantities
-    n_samples = x.shape[0]
-    a = np.zeros(n_samples, dtype=np.float64)
-    mdl = repeat(mdl, n_samples)
-    y_min = mdl.ym_.min(1, keepdims=True)
-    y_max = mdl.ym_.max(1, keepdims=True)
-    lb_: Array2d = lb[np.newaxis].repeat(n_samples, 0)  # type: ignore[index]
-    ub_: Array2d = ub[np.newaxis].repeat(n_samples, 0)  # type: ignore[index]
+    # create random generator and draw all the necessary random numbers
+    np_random = np.random.default_rng(
+        FIXED_SEED if common_random_numbers else seed  # overwrite seed by CRN
+    )
+    if antithetic_variates:
+        rng = _draw_normal(mc_iters // 2, horizon, n_samples, quasi_mc, np_random)
+        rng = np.concatenate((rng, -rng), 0)
+    else:
+        rng = _draw_normal(mc_iters, horizon, n_samples, quasi_mc, np_random)
 
-    # loop through the horizon
-    for h in range(horizon):
-        # compute the next point to query. If the strategy is "mpc", then the next point
-        # is just the next point in the trajectory. If the strategy is "rollout", then,
-        # for h=0, the next point is the input, and for h>0, the next point is the
-        # minimizer of the myopic acquisition function, i.e., base policy.
-        dym = y_max - y_min
-        if type == "mpc":
-            x_next = x[:, h, np.newaxis, :]
-        elif h == 0:  # type == "rollout"
-            x_next = x
-        else:  # type == "rollout"
-            x_next = vpso(
-                lambda x: myopic_acquisition(x, mdl, c1, c2, None, dym)[:, :, 0],
-                lb_,
-                ub_,
-                **pso_kwarg,
-                # TODO: pass a seed here.. Can be one of the quasi-random numbers
-            )[0][:, np.newaxis, :]
-
-        # predict the sampling of the next point
-        if deterministic:
-            y_hat = predict(mdl, x_next)
-        else:
-            raise NotImplementedError
-
-        # add to reward
-        a_h = myopic_acquisition(x_next, mdl, c1, c2, y_hat, dym)[:, 0, 0]
-        a += (discount**h) * a_h
-
-        # fit regression to new point, and update min/max
-        mdl = partial_fit(mdl, x_next, y_hat)
-        y_min = np.minimum(y_min, y_hat)
-        y_max = np.maximum(y_max, y_hat)
-    return a
+    # loop over MC iterations and return the average
+    # TODO: parallelize this loop with joblib (try threads and processes)
+    a = np.zeros(n_samples)
+    for i in range(mc_iters):
+        a += _compute_acquisition(
+            x,
+            mdl,
+            horizon,
+            discount,
+            c1,
+            c2,
+            lb,
+            ub,
+            n_samples,
+            rollout,
+            pso_kwargs,
+            np_random,
+            rng[i],
+        )
+    return a / mc_iters
