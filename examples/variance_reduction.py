b7127fb4da2f9c80a7fd6e77011e1f488070fab5
Filippo Airaldi <f.airaldi@tudelft.nl>
2023-06-25 15:43:04 +0200

    variance reduction example WIP

--- examples/variance_reduction.py
+++ examples/variance_reduction.py
@@ -44,54 +44,69 @@ kwargs = {
 }
 
 # compute the non-myopic acquisition for `x_target` with deterministic dynamics (without
-# Monte Carlo integration) and with stochastic dynamics (via MC)
-a_deterministic = deterministic_acquisition(**kwargs, seed=420).item()
-a_target = np.squeeze(
-    Parallel(n_jobs=-1, verbose=1)(
-        delayed(acquisition)(
-            **kwargs,
-            mc_iters=2**13,
-            quasi_mc=False,
-            common_random_numbers=False,
-            antithetic_variates=False,
-            parallel={"n_jobs": -1, "verbose": 1, "backend": "loky"},
-            return_as_list=True,
-            seed=420 + i,
+# Monte Carlo integration)
+seed = 420
+a_deterministic = deterministic_acquisition(**kwargs, seed=seed).item()
+
+# compute the acquisition with stochastic dynamics (via MC and no variance reduction).
+# Use a huge number of MC iterations to get a good estimate of the target.
+try:
+    # try to load the data from disk since these computations are quite long
+    data = np.load("examples/variance_reduction.npz")
+    a_target = data["a_target"]
+    a_qmc = data["a_qmc"]
+    print("Loaded data from disk.")
+
+except FileNotFoundError:
+    print("Computing data from scratch...")
+    N = 3
+
+    # run the computations
+    with Parallel(n_jobs=-1, backend="loky", verbose=1) as parallel:
+        a_target = np.squeeze(
+            parallel(
+                delayed(acquisition)(
+                    **kwargs,
+                    mc_iters=2**14,
+                    quasi_mc=False,
+                    common_random_numbers=False,  # CRN have no influece here
+                    antithetic_variates=False,
+                    parallel={"n_jobs": -1, "verbose": 1, "backend": "loky"},
+                    return_as_list=True,
+                    seed=seed + i,
+                )
+                for i in range(N)
+            )
         )
-        for i in range(3)
-    )
-)
+
+        # use Quasi-MC pseudo-random numbers
+        a_qmc = np.squeeze(
+            parallel(
+                delayed(acquisition)(
+                    **kwargs,
+                    mc_iters=2**11,
+                    quasi_mc=True,
+                    common_random_numbers=False,
+                    antithetic_variates=False,
+                    parallel={"n_jobs": -1, "verbose": 1, "backend": "loky"},
+                    return_as_list=True,
+                    seed=seed + i,
+                )
+                for i in range(N)
+            )
+        )
+
+    np.savez("examples/variance_reduction.npz", a_target=a_target, a_qmc=a_qmc)
 
 
-# TODO: ablation studies repeated for different seeds (target can be run only once)
-
-
-print(a_target.mean(-1))
-np.savez("examples/variance_reduction.npz", a_target=a_target)
-
-
-# # compute the non-myopic acquisition function for x with different variance reductions
-# kwargs["mc_iters"] = 2**7
-# a_no_vr = acquisition(
-#     **kwargs, quasi_mc=False, antithetic_variates=False, return_as_list=True
-# )
-# a_qmc = acquisition(
-#     **kwargs, quasi_mc=True, antithetic_variates=False, return_as_list=True
-# )
-# a_qmc_atv = acquisition(
-#     **kwargs, quasi_mc=True, antithetic_variates=True, return_as_list=True
-# )
-
-
-# # plotting
-# iters_ = np.arange(1, kwargs["mc_iters"] + 1)
-# _, ax = plt.subplots(1, 1, constrained_layout=True)
-# for a, lbl in zip(
-#     [a_no_vr, a_qmc, a_qmc_atv],
-#     ["Standard MC", "Quasi-MC", "Quasi-MC + Antithetic Variates"],
-# ):
-#     mean_estimate = np.cumsum(a) / iters_
-#     ax.semilogy(iters_, np.abs(mean_estimate - a_target), label=lbl)
-# ax.set_xlabel("MC iterations")
-# ax.set_ylabel("Estimation error")
-# plt.show()
+# plotting
+a_target_avg = a_target.mean(-1).mean(-1)
+_, ax = plt.subplots(1, 1, constrained_layout=True)
+for a, lbl in zip([a_target, a_qmc], ["Standard MC", "Quasi-MC"]):
+    iters = np.arange(1, a.shape[-1] + 1)
+    avg = np.cumsum(a, axis=-1) / iters.reshape(1, -1)
+    ax.semilogy(iters, np.abs(avg - a_target_avg), label=lbl)
+ax.set_xlabel("MC iterations")
+ax.set_ylabel("Estimation error")
+ax.set_xlim(2**7, 2**11)
+plt.show()
