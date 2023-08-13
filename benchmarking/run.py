6f526793f4ee7a3da5a75efbf17067e48e24515b
Filippo Airaldi <f.airaldi@tudelft.nl>
2023-08-13 22:37:23 +0200

    changed benchmark io to stream to csv

--- benchmarking/run.py
+++ benchmarking/run.py
@@ -6,12 +6,10 @@ problems.
 
 import argparse
 from datetime import datetime
-from itertools import groupby, product
+from itertools import product
+from multiprocessing import Lock, Pool
 
 import numpy as np
-from joblib import Parallel, delayed
-from scipy.io import savemat
-from vpso.typing import Array2d
 
 from globopt.core.problems import (
     get_available_benchmark_problems,
@@ -31,79 +29,91 @@ BENCHMARK_PROBLEMS = get_available_benchmark_problems()
 SIMPLE_PROBLEMS = get_available_simple_problems()
 
 
-def run_benchmark(problem_name: str, h: int, seed: int) -> tuple[list[float], float]:
-    """Solves the problem with the given horizon and seed, and returns as result the
-    performance of the algorithm in terms of best-so-far and total cost."""
-    problem, maxiter, regression_type = get_benchmark_problem(problem_name)
+def init_pool(this_lock: Lock) -> None:
+    """Initialize each process with a global variable lock."""
+    global lock
+    lock = this_lock
+
+
+def run_benchmarks_iteration(
+    problems: list[str], horizon: int, seed: int, output_csv: str
+) -> None:
+    """Runs one iteration of the benchmark problems with the given horizon and seed, and
+    saves as result the performance of the algorithm on each problem in terms of
+    best-so-far and total cost."""
+    c1, c2, eps = 1.5078, 1.4246, 1.0775
+    rollout = True
     bsf_callback = BestSoFarCallback()
     dp_callback = DpStageCostCallback()
     callbacks = CallbackCollection(bsf_callback, dp_callback)
-    c1, c2, eps = 1.5078, 1.4246, 1.0775
-    rollout = True
-    kwargs = {
-        "func": problem.f,
-        "lb": problem.lb,
-        "ub": problem.ub,
-        "mdl": Rbf(eps=eps / problem.dim) if regression_type == "rbf" else Idw(),
-        "init_points": problem.dim,
-        "c1": c1 / problem.dim,
-        "c2": c2 / problem.dim,
-        "maxiter": maxiter,
-        "seed": seed,
-        "callback": callbacks,
-        "pso_kwargs": {
-            "swarmsize": 5 * problem.dim * (rollout or h),
-            "xtol": 1e-9,
-            "ftol": 1e-9,
-            "maxiter": 300,
-            "patience": 10,
-        },
-    }
-    if h == 1:
-        _ = go(**kwargs)
-    else:
-        _ = nmgo(
-            horizon=h,
-            discount=0.9,
-            rollout=rollout,
-            mc_iters=0,
-            parallel=Parallel(n_jobs=1, verbose=0, backend="loky"),
-            **kwargs,
-        )
-    return bsf_callback, sum(dp_callback)
+    seeds = np.random.SeedSequence(seed).generate_state(len(problems))
+
+    for problem_name, seed_ in zip(problems, seeds):
+        # solve the problem
+        problem, maxiter, regression_type = get_benchmark_problem(problem_name)
+        kwargs = {
+            "func": problem.f,
+            "lb": problem.lb,
+            "ub": problem.ub,
+            "mdl": Rbf(eps=eps / problem.dim) if regression_type == "rbf" else Idw(),
+            "init_points": problem.dim,
+            "c1": c1 / problem.dim,
+            "c2": c2 / problem.dim,
+            "maxiter": maxiter,
+            "seed": seed_,
+            "callback": callbacks,
+            "pso_kwargs": {
+                "swarmsize": 5 * problem.dim * (rollout or horizon),
+                "xtol": 1e-9,
+                "ftol": 1e-9,
+                "maxiter": 300,
+                "patience": 10,
+            },
+        }
+        if horizon == 1:
+            _ = go(**kwargs)
+        else:
+            _ = nmgo(
+                horizon=horizon,
+                discount=0.9,
+                rollout=rollout,
+                mc_iters=0,
+                parallel=None,
+                **kwargs,
+            )
+
+        # write the result to a csv file
+        cost = sum(dp_callback)
+        bests = ",".join(map(str, bsf_callback))
+        with lock, open(output_csv, "a") as f:
+            f.write(f"{problem_name},{horizon},{cost},{bests}\n")
+
+        # clear the callbacks for the next problem
+        bsf_callback.clear()
+        dp_callback.clear()
 
 
 def run_benchmarks(
-    problems: list[str], horizons: list[int], trials: int, seed: int
-) -> dict[str, Array2d]:
+    problems: list[str], horizons: list[int], trials: int, seed: int, n_jobs: int
+) -> None:
     """Run the benchmarks for the given problems and horizons, repeated per the number
     of trials."""
     if problems == ["all"]:
         problems = BENCHMARK_PROBLEMS
+    seeds = np.random.SeedSequence(seed).generate_state(trials)
 
-    # seeds are independent of the horizons
-    N = len(problems)
-    seedseq = np.random.SeedSequence(seed)
-    seeds = dict(zip(problems, np.split(seedseq.generate_state(N * trials), N)))
-
-    def _run(name: str, h: int, trial: int) -> tuple[str, list[float]]:
-        seed_ = seeds[name][trial]
-        print(f"{name.upper()}: h={h}, iter={trial + 1} | seed={seed_}")
-        bsf, J = run_benchmark(name, h, seed_)
-        bsf.append(J)
-        return f"{name}_h{h}", bsf
-
-    data = Parallel(n_jobs=-1, verbose=100, backend="loky")(
-        delayed(_run)(name, h, trial)
-        for name, h, trial in product(problems, horizons, range(trials))
-    )
-    results: dict[str, Array2d] = {
-        k: np.asarray([e[1] for e in g]) for k, g in groupby(data, key=lambda o: o[0])
-    }
-
+    # create name of csv that will be filled with the results of each iteration and its
+    # lock to avoid writing to the file at the same time
     nowstr = datetime.now().strftime("%Y%m%d_%H%M%S")
-    savemat(f"sbm_{nowstr}.mat", results)
-    return results
+    csv_fname = f"results_{nowstr}.csv"
+    lock = Lock()
+
+    # launch each benchmarking iteration in parallel
+    with Pool(processes=n_jobs, initializer=init_pool, initargs=(lock,)) as pool:
+        pool.starmap(
+            run_benchmarks_iteration,
+            [(problems, h, s, csv_fname) for h, s in product(horizons, seeds)],
+        )
 
 
 if __name__ == "__main__":
@@ -129,8 +139,11 @@ if __name__ == "__main__":
     parser.add_argument(
         "--n-trials", type=int, default=30, help="Number of trials to run per problem."
     )
+    parser.add_argument(
+        "--n-jobs", type=int, default=1, help="Number of parallel processes."
+    )
     parser.add_argument("--seed", type=int, default=1909, help="RNG seed.")
     args = parser.parse_args()
 
     # run the benchmarks
-    results = run_benchmarks(args.problems, args.horizons, args.n_trials, args.seed)
+    run_benchmarks(args.problems, args.horizons, args.n_trials, args.seed, args.n_jobs)
