5cd6ef9ee1d08b3a86a66822da892252d37b9091
Filippo Airaldi <f.airaldi@tudelft.nl>
2023-12-10 16:16:26 +0100

    updated some tests

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -1,50 +1,14 @@
 import unittest
 
-import numpy as np
 import torch
 from parameterized import parameterized
-from scipy.io import loadmat
 
-from globopt.core_bt.problems import (
+from globopt.problems import (
     SimpleProblem,
     get_available_benchmark_problems,
     get_available_simple_problems,
     get_benchmark_problem,
 )
-from globopt.core_bt.regression import Idw, Rbf
-
-RESULTS = loadmat("tests/data_test_core.mat")
-
-
-def f(x):
-    return (
-        (1 + x * np.sin(2 * x) * np.cos(3 * x) / (1 + x**2)) ** 2
-        + x**2 / 12
-        + x / 10
-    )
-
-
-class TestRegression(unittest.TestCase):
-    def test_fit_and_partial_fit(self) -> None:
-        problem = SimpleProblem()
-        X = torch.as_tensor([-2.61, -1.92, -0.63, 0.38, 2], device="cpu").reshape(
-            1, -1, 1
-        )
-        Y = problem(X)
-        n = 3
-        mdls = [Idw(X[:, :n], Y[:, :n]), Rbf(X[:, :n], Y[:, :n], eps=0.5, svd_tol=0.0)]
-        for i in range(len(mdls)):
-            mdl = mdls[i]
-            if isinstance(mdl, Idw):
-                mdls[i] = Idw(X, Y)
-            else:
-                mdls[i] = Rbf(X, Y, mdl.eps, mdl.svd_tol, (mdl.Minv, mdl.coeffs))
-        x_hat = torch.linspace(-3, 3, 100).reshape(1, -1, 1)
-        y_hat = torch.cat([mdl(x_hat).mean for mdl in mdls]).squeeze(-1)
-        torch.testing.assert_close(
-            y_hat, torch.as_tensor(RESULTS["y_hat"][:2], dtype=y_hat.dtype)
-        )
-
 
 EXPECTED_F_OPT: dict[str, float] = {
     # AnotherSimple1dProblem.f.__name__[1:]: -0.669169468,
---
+++ tests/test_regression.py
@@ -0,0 +1,37 @@
+import unittest
+
+import torch
+from scipy.io import loadmat
+
+from globopt.problems import SimpleProblem
+from globopt.regression import Idw, Rbf
+
+RESULTS = loadmat("tests/data_test_core.mat")
+
+
+class TestRegression(unittest.TestCase):
+    def test_fit_and_partial_fit(self) -> None:
+        problem = SimpleProblem()
+        X = torch.as_tensor([-2.61, -1.92, -0.63, 0.38, 2], device="cpu").reshape(
+            1, -1, 1
+        )
+        Y = problem(X)
+        n = 3
+        mdls = [Idw(X[:, :n], Y[:, :n]), Rbf(X[:, :n], Y[:, :n], eps=0.5, svd_tol=0.0)]
+        for i in range(len(mdls)):
+            mdl = mdls[i]
+            if isinstance(mdl, Idw):
+                mdls[i] = Idw(X, Y)
+            else:
+                mdls[i] = Rbf(X, Y, mdl.eps, mdl.svd_tol, (mdl.Minv, mdl.coeffs))
+        x_hat = torch.linspace(-3, 3, 100).reshape(1, -1, 1)
+        y_hat = torch.cat([mdl(x_hat).mean for mdl in mdls]).squeeze(-1)
+        torch.testing.assert_close(
+            y_hat, torch.as_tensor(RESULTS["y_hat"][:2], dtype=y_hat.dtype)
+        )
+        # NOTE: we only take the first 2 rows of y_hat because the third was computed
+        # with a kernel that was later removed.
+
+
+if __name__ == "__main__":
+    unittest.main()
