1bc85107da02f124c994f9010bde66d0fabc72c0
Filippo Airaldi <f.airaldi@tudelft.nl>
2024-01-28 18:45:23 +0100

    implemented a lot of new problems to be tested

--- src/globopt/problems.py
+++ src/globopt/problems.py
@@ -15,8 +15,11 @@ References
 [4] Wang, Z. and Jegelka, S., 2017, July. Max-value entropy search for efficient
     Bayesian optimization. In International Conference on Machine Learning
     (pp. 3627-3635). PMLR.
+[5] Eric, B., Freitas, N. and Ghosh, A., 2007. Active preference learning with discrete
+    choice data. Advances in neural information processing systems, 20.
 """
 
+from functools import partial
 from pathlib import Path
 from types import MappingProxyType
 from typing import Any, Literal, Union
@@ -26,6 +29,9 @@ import torch
 from botorch.test_functions import (
     Ackley,
     Branin,
+    DropWave,
+    EggHolder,
+    Griewank,
     Hartmann,
     Rastrigin,
     Rosenbrock,
@@ -101,6 +107,181 @@ class Step2(SyntheticTestFunction):
 
     def evaluate_true(self, X: Tensor) -> Tensor:
         return (X + 0.5).floor().square().sum(-1)
+
+
+class Himmelblau(SyntheticTestFunction):
+    r"""Himmelblau function, a 2-dimensional synthetic test function given by:
+
+        f(x) = (x1^2 + x2 - 11)^2 + (x1 + x2^2 - 7)^2.
+
+    x is bounded [-5,5] in each dimension. f has 4 global minima at
+    `x_opt = (3, 2), (-2.80511, 3.13131), (-3.77931, -3.28318), (3.58442, -1.84812)`
+    with `f_opt = 0`.
+    """
+
+    dim = 2
+    _optimal_value = 0.0
+    _optimizers = [
+        (3.0, 2.0),
+        (-2.805118, 3.131312),
+        (-3.779310, -3.283186),
+        (3.584428, -1.848126),
+    ]
+    _bounds = [(-5.0, 5.0), (-5.0, 5.0)]
+
+    def evaluate_true(self, X: Tensor) -> Tensor:
+        x1 = X[..., 0]
+        x2 = X[..., 1]
+        return (x1.square() + x2 - 11).square() + (x1 + x2.square() - 7).square()
+
+
+class Brochu(SyntheticTestFunction):
+    r"""Brochu function, a 2-, 4-, or 6-dimensional synthetic test function given by:
+
+        g(x) = sum_i sin(x_i) + x_i / 3 + sin(12 * x_i)
+        f2(x) = -max(g(x) - 1, 0)
+        f4(x) = -g(x)
+        f6(x) = -g(x)
+
+    x is bounded [0,1] in each dimension. f has the following minimizer
+    `x_opt_i = 0.6623009251970219` and optimal values  `f_opt2 = -2.662639755973945`,
+    `f_opt4 = -7.32527951194789`, and `f_opt6 = -10.987919267921836`.
+    """
+
+    def __init__(self, dim: int, *args: Any, **kwargs: Any) -> None:
+        if dim not in (2, 4, 6):
+            raise ValueError(f"Brochu with dim {dim} not defined")
+        self.dim = dim
+        self._optimizers = [(0.6623009251970219,) * dim]
+        if dim == 2:
+            self._optimal_value = -2.662639755973945
+        elif dim == 4:
+            self._optimal_value = -7.32527951194789
+        else:
+            self._optimal_value = -10.987919267921836
+        super().__init__(bounds=[(0.0, 1.0) for _ in range(dim)], *args, **kwargs)
+
+    def evaluate_true(self, X: Tensor) -> Tensor:
+        g = (X.sin() + X / 3 + (12 * X).sin()).sum(dim=-1)
+        return -torch.fmax(g - 1.0, torch.scalar_tensor(0.0)) if self.dim == 2 else -g
+
+
+class GoldsteinPrice(SyntheticTestFunction):
+    r"""Goldstein-Price function, a 2-dimensional synthetic test function given by:
+
+        g(x) = 1 + (x1 + x2 + 1)^2 (19 - 14*x1 + 3*x1^2 - 14*x2 + 6*x1*x2 + 3*x2^2)
+        p(x) = 30 + (2*x1 - 3*x2)^2 (18 - 32*x1 + 12*x1^2 + 48*x2 - 36*x1*x2 + 27*x2^2)
+        f(x) = g(x) p(x).
+
+    x is bounded [-2,2] in each dimension. f has a global minimum at `x_opt = (0, -1)`
+    with `f_opt = 3`.
+    """
+
+    dim = 2
+    _optimal_value = 3.0
+    _optimizers = [(0.0, -1.0)]
+    _bounds = [(-2.0, 2.0), (-2.0, 2.0)]
+
+    def evaluate_true(self, X: Tensor) -> Tensor:
+        x1 = X[..., 0]
+        x2 = X[..., 1]
+        x1sq = x1.square()
+        x2sq = x2.square()
+        x12 = x1.mul(x2)
+        g = 1 + (x1 + x2 + 1).square() * (
+            19 - 14 * x1 + 3 * x1sq - 14 * x2 + 6 * x12 + 3 * x2sq
+        )
+        p = 30 + (2 * x1 - 3 * x2).square() * (
+            18 - 32 * x1 + 12 * x1sq + 48 * x2 - 36 * x12 + 27 * x2sq
+        )
+        return p * g
+
+
+class Bohachevsky(SyntheticTestFunction):
+    r"""Bohachevsky function, a 2-dimensional synthetic test function given by:
+
+        f(x) = x1^2 + 2*x2^2 - 0.3*cos(3*pi*x1) - 0.4*cos(4*pi*x2) + 0.7.
+
+    x is bounded [-100,100] in each dimension. f has a global minimum at
+    `x_opt = (0, 0)` with `f_opt = 0.0`.
+    """
+
+    dim = 2
+    _optimal_value = 0.0
+    _optimizers = [(0.0, 0.0)]
+    _bounds = [(-100.0, 100.0), (-100.0, 100.0)]
+
+    def evaluate_true(self, X: Tensor) -> Tensor:
+        x1 = X[..., 0]
+        x2 = X[..., 1]
+        return (
+            x1.square()
+            + 2 * x2.square()
+            - 0.3 * (3 * torch.pi * x1).cos()
+            - 0.4 * (4 * torch.pi * x2).cos()
+            + 0.7
+        )
+
+
+class Shubert(SyntheticTestFunction):
+    r"""Shubert function, a 2-dimensional synthetic test function given by:
+
+        f(x) = prod_i sum_j cos((j + 1) * x_i + j).
+
+    x is bounded [-5.12,5.12] in each dimension. f has 18 global minima at with
+    `f_opt = -186.7309`."""
+
+    dim = 2
+    _optimal_value = -186.7309
+    _optimizers = [
+        (-7.0835, 4.858),
+        (-7.0835, -7.7083),
+        (-1.4251, -7.0835),
+        (5.4828, 4.858),
+        (-1.4251, -0.8003),
+        (4.858, 5.4828),
+        (-7.7083, -7.0835),
+        (-7.0835, -1.4251),
+        (-7.7083, -0.8003),
+        (-7.7083, 5.4828),
+        (-0.8003, -7.7083),
+        (-0.8003, -1.4251),
+        (-0.8003, 4.858),
+        (-1.4251, 5.4828),
+        (5.4828, -7.7083),
+        (4.858, -7.0835),
+        (5.4828, -1.4251),
+        (4.858, -0.8003),
+    ]
+    _bounds = [(-5.12, 5.12), (-5.12, 5.12)]
+
+    def evaluate_true(self, X: Tensor) -> Tensor:
+        ndim = X.ndim - 1
+        I = torch.arange(1, 6, dtype=X.dtype, device=X.device).view(5, *(1,) * ndim)
+        Ip1 = I + 1
+        p1 = torch.cos(Ip1 * X[..., 0].unsqueeze(0) + I).mul(I).sum(dim=0)
+        p2 = torch.cos(Ip1 * X[..., 1].unsqueeze(0) + I).mul(I).sum(dim=0)
+        return p1 * p2
+
+
+class Bukin(SyntheticTestFunction):
+    r"""Bukin function, a 2-dimensional synthetic test function given by:
+
+        f(x) = 100 * sqrt(abs(x2 - 0.01 * x1^2)) + 0.01 * abs(x1 + 10).
+
+    x is bounded [-15,-5] in the first dimension and [-3,3] in the second dimension.
+    f has a global minimum at `x_opt = (-10, 1)` with `f_opt = 0.0`.
+    """
+
+    dim = 2
+    _optimal_value = 0.0
+    _optimizers = [(-10.0, 1.0)]
+    _bounds = [(-15.0, -5.0), (-3.0, 3.0)]
+
+    def evaluate_true(self, X: Tensor) -> Tensor:
+        x1 = X[..., 0]
+        x2 = X[..., 1]
+        return 100.0 * (x2 - 0.01 * x1.square()).abs().sqrt() + 0.01 * (x1 + 10.0).abs()
 
 
 class HyperTuningGridTestFunction(SyntheticTestFunction):
@@ -192,16 +373,48 @@ class Svm(HyperTuningGridTestFunction):
         super().__init__("benchmarking/data/svm_on_grid.csv", *args, **kwargs)
 
 
+Ackley2 = partial(Ackley, dim=2)
+Ackley2.__name__ = Ackley.__name__ + "2"
+Ackley5 = partial(Ackley, dim=5)
+Ackley5.__name__ = Ackley.__name__ + "5"
+Brochu2 = partial(Brochu, dim=2)
+Brochu2.__name__ = Brochu.__name__ + "2"
+Brochu4 = partial(Brochu, dim=4)
+Brochu4.__name__ = Brochu.__name__ + "4"
+Brochu6 = partial(Brochu, dim=6)
+Brochu6.__name__ = Brochu.__name__ + "6"
+Hartmann3 = partial(Hartmann, dim=3)
+Hartmann3.__name__ = Hartmann.__name__ + "3"
+Hartmann6 = partial(Hartmann, dim=6)
+Hartmann6.__name__ = Hartmann.__name__ + "6"
+Shekel5 = partial(Shekel, m=5)
+Shekel5.__name__ = Shekel.__name__ + "5"
+Shekel7 = partial(Shekel, m=7)
+Shekel7.__name__ = Shekel.__name__ + "7"
+
+
 TESTS: dict[
     str, tuple[type[SyntheticTestFunction], dict[str, Any], int, Literal["rbf", "idw"]]
 ] = MappingProxyType(
     {
         problem.__name__.lower(): (problem, kwargs, max_evals, regressor_type)
         for problem, kwargs, max_evals, regressor_type in [
-            (Ackley, {}, 50, "idw"),
+            (Ackley2, {}, 80, "idw"),
+            (Ackley5, {}, 50, "idw"),
             (Adjiman, {}, 20, "idw"),
+            (Bohachevsky, {}, 25, "idw"),
             (Branin, {}, 40, "idw"),
-            (Hartmann, {"dim": 3}, 50, "rbf"),
+            (Brochu2, {}, 50, "idw"),
+            (Brochu4, {}, 80, "idw"),
+            (Brochu6, {}, 80, "idw"),
+            (Bukin, {}, 40, "idw"),
+            (DropWave, {}, 40, "idw"),
+            (EggHolder, {}, 40, "idw"),
+            (GoldsteinPrice, {}, 25, "idw"),
+            (Griewank, {"dim": 3}, 35, "rbf"),
+            (Hartmann3, {}, 50, "rbf"),
+            (Hartmann6, {}, 100, "rbf"),
+            (Himmelblau, {}, 25, "rbf"),
             (Lda, {}, 30, "idw"),
             (LogReg, {}, 30, "idw"),
             (NnBoston, {}, 100, "rbf"),
@@ -210,7 +423,9 @@ TESTS: dict[
             (RobotPush3, {}, 90, "idw"),
             (RobotPush4, {}, 100, "rbf"),
             (Rosenbrock, {"dim": 8}, 50, "rbf"),
-            (Shekel, {"m": 7}, 80, "rbf"),
+            (Shekel5, {}, 80, "rbf"),
+            (Shekel7, {}, 100, "rbf"),
+            (Shubert, {}, 25, "rbf"),
             (SixHumpCamel, {"bounds": [(-5.0, 5.0), (-5.0, 5.0)]}, 10, "rbf"),
             (Step2, {"dim": 5}, 80, "idw"),
             (StyblinskiTang, {"dim": 5}, 60, "rbf"),
--- tests/test_problems.py
+++ tests/test_problems.py
@@ -4,10 +4,22 @@ import torch
 from parameterized import parameterized
 
 from globopt.problems import (
-    Ackley,
+    Ackley2,
+    Ackley5,
     Adjiman,
+    Bohachevsky,
     Branin,
-    Hartmann,
+    Brochu2,
+    Brochu4,
+    Brochu6,
+    Bukin,
+    DropWave,
+    EggHolder,
+    GoldsteinPrice,
+    Griewank,
+    Hartmann3,
+    Hartmann6,
+    Himmelblau,
     HyperTuningGridTestFunction,
     Lda,
     LogReg,
@@ -17,8 +29,9 @@ from globopt.problems import (
     RobotPush3,
     RobotPush4,
     Rosenbrock,
-    Shekel,
-    SimpleProblem,
+    Shekel5,
+    Shekel7,
+    Shubert,
     SixHumpCamel,
     Step2,
     StyblinskiTang,
@@ -28,24 +41,37 @@ from globopt.problems import (
 )
 
 CLS: list[type, float] = [
-    (SimpleProblem, 0.279504),
-    (Ackley, 0.0),
+    (Ackley2, 0.0),
+    (Ackley5, 0.0),
     (Adjiman, -2.02181),
-    (Branin, 0.3978873),
-    (Hartmann, -3.86278214782076),
+    (Bohachevsky, 0.0),
+    (Branin, 0.3978873),  # 5
+    (Brochu2, -2.662639755973945),
+    (Brochu4, -7.32527951194789),
+    (Brochu6, -10.987919267921836),
+    (Bukin, 0.0),
+    (DropWave, -1.0),  # 10
+    (EggHolder, -959.6407),
+    (GoldsteinPrice, 3.0),
+    (Griewank, 0.0),
+    (Hartmann3, -3.86278214782076),
+    (Hartmann6, -3.32236801141551),  # 15
+    (Himmelblau, 0.0),
     (Lda, 1266.17),
     (LogReg, 0.0685),
     (NnBoston, 6.5212),
-    (NnCancer, 0.040576),
+    (NnCancer, 0.040576),  # 20
     (Rastrigin, 0.0),
     (RobotPush3, 0.074788),
     (RobotPush4, 0.076187),
     (Rosenbrock, 0.0),
-    (Shekel, -10.4029),
-    (Step2, 0.0),
+    (Shekel5, -10.1532),
+    (Shekel7, -10.4029),  # 25
+    (Shubert, -186.7309),
     (SixHumpCamel, -1.0316),
+    (Step2, 0.0),
     (StyblinskiTang, -39.16599 * 5.0),
-    (Svm, 0.2411),
+    (Svm, 0.2411),  # 30
 ]
 EXPECTED_F_OPT: dict[str, float] = {cls.__name__.lower(): f_opt for cls, f_opt in CLS}
 
@@ -65,17 +91,19 @@ class TestProblems(unittest.TestCase):
         expected = EXPECTED_F_OPT[name]
 
         actual = problem._optimal_value
-        torch.testing.assert_close(actual, expected, rtol=1e-4, atol=1e-6, msg=name)
+        torch.testing.assert_close(
+            actual, expected, rtol=1e-4, atol=1e-6, msg=f"{name} f_opt"
+        )
 
         if (
             not isinstance(problem, HyperTuningGridTestFunction)
             and problem._optimizers is not None
         ):
-            for x_opt in problem._optimizers:
+            for i, x_opt in enumerate(problem._optimizers):
                 f_computed = problem(torch.as_tensor(x_opt))
                 expected_ = torch.as_tensor(expected).view_as(f_computed).to(f_computed)
                 torch.testing.assert_close(
-                    f_computed, expected_, rtol=1e-4, atol=1e-6, msg=name
+                    f_computed, expected_, rtol=1e-4, atol=1e-6, msg=f"{name} x_opt {i}"
                 )
 
 
