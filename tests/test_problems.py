31ddf9ef4d0dc195bea0943e0081f0abe946223d
Filippo Airaldi <filippoairaldi@gmail.com>
2025-05-12 16:22:07 +0200

    fixing tests

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -13,6 +13,7 @@ from globopt.problems import (
     Brochu4,
     Brochu6,
     Bukin,
+    Cosmological,
     DropWave,
     EggHolder,
     GoldsteinPrice,
@@ -32,6 +33,7 @@ from globopt.problems import (
     Shekel5,
     Shekel7,
     Shubert,
+    SimpleProblem,
     SixHumpCamel,
     Step2,
     StyblinskiTang,
@@ -45,35 +47,41 @@ CLS: list[type, float] = [
     (Ackley5, 0.0),
     (Adjiman, -2.02181),
     (Bohachevsky, 0.0),
-    (Branin, 0.3978873),  # 5
+    (Branin, 0.3978873),
     (Brochu2, -2.662639755973945),
     (Brochu4, -7.32527951194789),
     (Brochu6, -10.987919267921836),
     (Bukin, 0.0),
-    (DropWave, -1.0),  # 10
+    (Cosmological, 12.103),
+    (DropWave, -1.0),
     (EggHolder, -959.6407),
     (GoldsteinPrice, 3.0),
     (Griewank, 0.0),
     (Hartmann3, -3.86278214782076),
-    (Hartmann6, -3.32236801141551),  # 15
+    (Hartmann6, -3.32236801141551),
     (Himmelblau, 0.0),
     (Lda, 1266.17),
     (LogReg, 0.0685),
     (NnBoston, 6.5212),
-    (NnCancer, 0.040576),  # 20
+    (NnCancer, 0.040576),
     (Rastrigin, 0.0),
     (RobotPush3, 0.074788),
     (RobotPush4, 0.076187),
     (Rosenbrock, 0.0),
     (Shekel5, -10.1532),
-    (Shekel7, -10.4029),  # 25
+    (Shekel7, -10.4029),
     (Shubert, -186.7309),
     (SixHumpCamel, -1.0316),
     (Step2, 0.0),
+    (SimpleProblem, 0.279504),
     (StyblinskiTang, -39.16599 * 5.0),
-    (Svm, 0.2411),  # 30
+    (Svm, 0.2411),
 ]
 EXPECTED_F_OPT: dict[str, float] = {cls.__name__.lower(): f_opt for cls, f_opt in CLS}
+
+
+def name_func(func, num, p):
+    return f"{func.__name__}_{num}_{p.args[0].__name__}"
 
 
 class TestProblems(unittest.TestCase):
@@ -81,7 +89,7 @@ class TestProblems(unittest.TestCase):
         problems = get_available_benchmark_problems()
         self.assertListEqual(problems, sorted(problems))
 
-    @parameterized.expand([(cls,) for cls, _ in CLS])
+    @parameterized.expand([(cls,) for cls, _ in CLS], name_func=name_func)
     def test_optimal_value_and_point(self, cls: type):
         name = cls.__name__.lower()
         try:
